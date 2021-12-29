a97e0d9571a31d1a8b82d03ec6d6332e856de26a
markmead <itsmarkmead@gmail.com>
2021-12-29 17:07:27 +0000

    Update card

--- components/card.tsx
+++ components/card.tsx
@@ -13,8 +13,8 @@ const Card: FunctionComponent<Props> = ({ component }) => {
 
   return (
     <Link href={`/components/${id}`}>
-      <a className="p-8 border-2 border-black">
-        <h2 className="text-lg font-medium">{title}</h2>
+      <a className="p-8 border-2 border-black rounded-lg hover:opacity-75">
+        <h2 className="mt-1 text-lg font-medium">{title}</h2>
         <p className="mt-1 text-sm">{description}</p>
       </a>
     </Link>
